bc91c390c62cdf71992faff9b122c54b2bb32fa3
Wouter Peetermans <wouter@wouterpeetermans.com>
2022-02-13 23:07:03 +0100

    Add print name functionality to program

--- program.sh
+++ program.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "Hello World!";
+<<<<<<< HEAD
 echo "Feature 2";
+=======
+read -p "Give your name: " name;
+echo $name;
+>>>>>>> 5bd20d8 (Add print name functionality to program)
